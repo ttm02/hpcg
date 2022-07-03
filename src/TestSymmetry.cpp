704602aa95950bbfc77633c2706411ad2ee3f9ec
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-07-03 20:35:55 +0200

    begin halo for other usages of SPMV

--- src/TestSymmetry.cpp
+++ src/TestSymmetry.cpp
@@ -82,7 +82,8 @@ int TestSymmetry(SparseMatrix & A, Vector & b, Vector & xexact, TestSymmetryData
 
  // Next, compute x'*A*y
  ComputeDotProduct(nrow, y_ncol, y_ncol, yNorm2, t4, A.isDotProductOptimized);
- ExchangeHalo(A, y_ncol);
+ BeginExchangeHaloSend(A, y_ncol);
+ BeginExchangeHaloRecv(A, y_ncol);
  int ierr = ComputeSPMV(A, y_ncol, z_ncol); // z_nrow = A*y_overlap
  if (ierr) HPCG_fout << "Error in call to SpMV: " << ierr << ".\n" << endl;
  double xtAy = 0.0;
@@ -91,7 +92,8 @@ int TestSymmetry(SparseMatrix & A, Vector & b, Vector & xexact, TestSymmetryData
 
  // Next, compute y'*A*x
  ComputeDotProduct(nrow, x_ncol, x_ncol, xNorm2, t4, A.isDotProductOptimized);
- ExchangeHalo(A, x_ncol);
+ BeginExchangeHaloSend(A, x_ncol);
+ BeginExchangeHaloRecv(A, x_ncol);
  ierr = ComputeSPMV(A, x_ncol, z_ncol); // b_computed = A*x_overlap
  if (ierr) HPCG_fout << "Error in call to SpMV: " << ierr << ".\n" << endl;
  double ytAx = 0.0;
@@ -127,7 +129,8 @@ int TestSymmetry(SparseMatrix & A, Vector & b, Vector & xexact, TestSymmetryData
  int numberOfCalls = 2;
  double residual = 0.0;
  for (int i=0; i< numberOfCalls; ++i) {
-	 ExchangeHalo(A, x_ncol);
+	 BeginExchangeHaloSend(A, x_ncol);
+	 BeginExchangeHaloRecv(A, x_ncol);
    ierr = ComputeSPMV(A, x_ncol, z_ncol); // b_computed = A*x_overlap
    if (ierr) HPCG_fout << "Error in call to SpMV: " << ierr << ".\n" << endl;
    if ((ierr = ComputeResidual(A.localNumberOfRows, b, z_ncol, residual)))
